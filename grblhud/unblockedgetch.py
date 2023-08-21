ca23b9cfa80a56eca21072951c8744cc8a2f0cc3
johannesnoordanus <52376919+johannesnoordanus@users.noreply.github.com>
2023-08-21 21:29:51 +0200

    Version 1.4.0
    Added function getch_nowait

--- grblhud/unblockedgetch.py
+++ grblhud/unblockedgetch.py
@@ -72,6 +72,32 @@ class UnblockedGetch:
 
         return chr(c[0])
 
+    def getch_nowait(self):
+        """
+        unbuffered, unblocked, nowait, raw (uncooked) character input
+        """
+        # cbreak mode
+        self.set_stdin_cbreak()
+
+        c = b''
+        if self.stdinHasData():
+
+                # Input in cbreak mode seems to be buffered. When reading the arrow keys (for example)
+                # As a consequence of the three byte encoding of the arrow keys only one byte can be
+                # received (esc) and the rest gets 'stuck' until other key(s) are entered (and the two
+                # bytes left come out). After a lot of tests my conclusion is that none of the solutions
+                # given on the internet seem to work. Changing stdin buffering to 0, flushing(?), etc
+                # does not work. However, switching to the solution below (raw mode) in cbreak did work:
+
+                # https://stackoverflow.com/questions/75218737/python-sys-stdin-buffer-size-detection:
+            c = sys.stdin.buffer.read1(1)
+                # c = sys.stdin.buffer.raw.read(10) # Note this indeed does not block for 3 byte
+                                                    # key input (<-, ->, ^, down arrow) for example.
+        # clear cbreak
+        self.restore_stdin_io()
+
+        return chr(c[0]) if c != b'' else ''
+
 def main():
     print("type 'z' to exit!")
     unblkgetch = UnblockedGetch().getch
